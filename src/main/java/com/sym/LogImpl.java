088d6e2b822af8413ea5265faae557ffb73f4121
沈燕明 <542569199@qq.com>
2021-05-24 08:53:29 +0800

    log

--- src/main/java/com/sym/LogImpl.java
+++ src/main/java/com/sym/LogImpl.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
  */
 public class LogImpl implements Log {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(LogImpl.class);
+    private Logger log;
 
     public LogImpl() {
         this("");
@@ -22,16 +22,17 @@ public class LogImpl implements Log {
 
     public LogImpl(String name) {
         // 兼容tomcat日志
+        log = LoggerFactory.getLogger(name);
     }
 
     @Override
     public boolean isDebugEnabled() {
-        return LOGGER.isDebugEnabled();
+        return log.isDebugEnabled();
     }
 
     @Override
     public boolean isErrorEnabled() {
-        return LOGGER.isErrorEnabled();
+        return log.isErrorEnabled();
     }
 
     @Override
@@ -41,76 +42,76 @@ public class LogImpl implements Log {
 
     @Override
     public boolean isInfoEnabled() {
-        return LOGGER.isInfoEnabled();
+        return log.isInfoEnabled();
     }
 
     @Override
     public boolean isTraceEnabled() {
-        return LOGGER.isTraceEnabled();
+        return log.isTraceEnabled();
     }
 
     @Override
     public boolean isWarnEnabled() {
-        return LOGGER.isWarnEnabled();
+        return log.isWarnEnabled();
     }
 
     @Override
     public void trace(Object message) {
-        LOGGER.trace("{}", message);
+        log.trace("{}", message);
     }
 
     @Override
     public void trace(Object message, Throwable t) {
-        LOGGER.trace("{}", message, t);
+        log.trace("{}", message, t);
     }
 
     @Override
     public void debug(Object message) {
-        LOGGER.debug("{}", message);
+        log.debug("{}", message);
     }
 
     @Override
     public void debug(Object message, Throwable t) {
-        LOGGER.debug("{}", message, t);
+        log.debug("{}", message, t);
     }
 
     @Override
     public void info(Object message) {
-        LOGGER.info("{}", message);
+        log.info("{}", message);
     }
 
     @Override
     public void info(Object message, Throwable t) {
-        LOGGER.info("{}", message, t);
+        log.info("{}", message, t);
     }
 
     @Override
     public void warn(Object message) {
-        LOGGER.warn("{}", message);
+        log.warn("{}", message);
     }
 
     @Override
     public void warn(Object message, Throwable t) {
-        LOGGER.warn("{}", message, t);
+        log.warn("{}", message, t);
     }
 
     @Override
     public void error(Object message) {
-        LOGGER.error("{}", message);
+        log.error("{}", message);
     }
 
     @Override
     public void error(Object message, Throwable t) {
-        LOGGER.error("{}", message, t);
+        log.error("{}", message, t);
     }
 
     @Override
     public void fatal(Object message) {
-        LOGGER.error("{}", message);
+        log.error("{}", message);
     }
 
     @Override
     public void fatal(Object message, Throwable t) {
-        LOGGER.error("{}", message, t);
+        log.error("{}", message, t);
     }
 }
